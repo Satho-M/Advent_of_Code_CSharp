7eab6e9ffac237ab7ce03afb50ca35cd3c9d2033
Satho <mehsatho@gmail.com>
2021-12-07 19:29:34 +0000

    Initial commit

---
+++ AOCLibrary/Reader.cs
@@ -0,0 +1,19 @@
+﻿    namespace Advent_of_Code_Library
+    {
+        public class Reader
+        {
+            public IEnumerable<string> ReadInput(string input)
+            {
+                string[] output = File.ReadAllLines(@"..\..\..\" + input);
+
+                return output;
+            }
+
+            public IEnumerable<string> CleanSpaces(IEnumerable<string> input)
+            {
+                IEnumerable<string> output = input.Where(l => !String.IsNullOrEmpty(l)).ToList();
+
+                return output;
+            }
+        }
+    }
